0bbadcefd4e6bdd1cbf41d088e37bdeb47b1b354
fnn <garx.g@proton.me>
2023-08-16 20:35:12 +0200

    movement

--- Project1/Assets/Scripts/PlayerMovement.cs
+++ Project1/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    public float speed = 0;
+    private Rigidbody2D rb;
     
     void Start()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
    
     void Update()
     {
+        float dirx = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(dirx * speed, rb.velocity.y);
         
+
     }
 }
