395ba648308ef03eb2868652b538422947b6e167
fnn <garx.g@proton.me>
2023-08-17 14:46:29 +0200

    Playermovement changes

--- Project1/Assets/Scripts/PlayerMovement.cs
+++ Project1/Assets/Scripts/PlayerMovement.cs
@@ -12,6 +12,7 @@ public class PlayerMovement : MonoBehaviour
 
 
     private Rigidbody2D rb;
+    public LayerMask isGround;
     
     void Start()
     {
@@ -25,7 +26,16 @@ public class PlayerMovement : MonoBehaviour
 
         rb.velocity = new Vector2(dirx * speed, rb.velocity.y);
 
-        if (Input.GetButton("Jump"))
+
+        if(dirx != 0 && dirx  > 0)
+        {
+            flip(180);
+        } else if(dirx != 0 && dirx < 0)
+        {
+            flip(0);
+        }
+
+            if (Input.GetButton("Jump") && ground())
         {
            
                 rb.velocity = new Vector2(rb.velocity.x, jumpForce);
@@ -37,4 +47,36 @@ public class PlayerMovement : MonoBehaviour
 
 
     }
+
+
+
+
+
+
+
+
+    private bool ground()
+    {
+        BoxCollider2D coll = gameObject.GetComponent<BoxCollider2D>();
+
+        return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, isGround);
+
+    }
+
+
+
+    void flip(float degree)
+    {
+
+        float rotationY;
+
+        rotationY = degree;
+           
+            //transform.Rotate(0f, 180f, 0f);
+            transform.rotation = Quaternion.Euler(transform.rotation.x, rotationY, 0);
+        
+    }
+
+
+
 }
